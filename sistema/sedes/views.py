7b334d8ab38e7b6a36d6b3e64f3d1068b56aa182
Gabriel Teixeira <gab.m.t09@gmail.com>
2020-06-18 18:17:12 -0300

    Excluir sedes

--- sistema/sedes/views.py
+++ sistema/sedes/views.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for
 from sistema import app, db, allowed_image
 from sistema.sedes.models import Sede
 from werkzeug.utils import secure_filename
@@ -11,11 +11,6 @@ sedes = Blueprint('sedes', __name__, template_folder="templates")
 def index():
     sedes = Sede.query.order_by(Sede.id.desc()).all()
     return render_template('sedes.html', sedes_front=sedes)
-
-
-@sedes.route('/sede_esp/')
-def sede_esp():
-    return render_template('sede_esp.html')
 
 
 @sedes.route('/adicionar-sede/', methods=['GET', 'POST'])
@@ -43,8 +38,21 @@ def adicionar_sede():
     else:
         return render_template('adicionar-sede.html')
 
+
 @sedes.route('/sede-especifica/<_id>', methods=['GET', 'POST'])
 def sede_especifica(_id):
     sede = Sede.query.get_or_404(_id)
 
-    return render_template('sede_esp.html', sede = sede)
+    return render_template('sede_esp.html', sede=sede)
+
+
+@sedes.route('/excluir_sede/<_id>', methods=['GET', 'POST'])
+def excluir_sede(_id):
+    sede = Sede.query.get_or_404(_id)
+
+    if request.method == 'POST':
+        db.session.delete(sede)
+        db.session.commit()
+
+        return redirect(url_for('sedes.index'))
+    return render_template('excluir_sede.html', sede=sede)
