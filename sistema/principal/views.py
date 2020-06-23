f44262141e0999d76138dcebbcbace8a4ead7dae
Rafael Santos <>
2020-06-23 19:47:41 -0300

    pesquisa por genero

--- sistema/principal/views.py
+++ sistema/principal/views.py
@@ -46,10 +46,18 @@ def pesquisa():
     #resultados de livros pesquisados
     search_livros_title = Livro.query.filter(Livro.title.contains(str(pesquisa).strip().title())).all()
     search_livros_author = Livro.query.filter(Livro.author.contains(str(pesquisa).strip().title())).all()
+    search_livros_gender = Livro.query.filter(Livro.gender.contains(str(pesquisa).strip().title())).all()
+
     titulo_l = set(search_livros_title)
     autor_l = set(search_livros_author)
-    interc_l = autor_l - titulo_l
-    livros_pesquisados =  list(interc_l) + search_livros_title
+    genero_l = set(search_livros_gender)
+
+    interc_l1 = autor_l - genero_l
+    apenas_autor = list(interc_l1) + search_livros_gender
+    autor_e_genero_l = set(apenas_autor)
+    interc_l2 = autor_e_genero_l - titulo_l
+
+    livros_pesquisados =  list(interc_l2) + search_livros_title
 
     #resultados de sedes pesquisados
     search_sedes_nome = Sede.query.filter(Sede.name.contains(str(pesquisa).strip().title())).all()
@@ -65,7 +73,7 @@ def pesquisa():
     nome_s = set(search_sedes_nome)
     endereco_s = set(search_sede_endereco)
     interc_s = endereco_s - nome_s
-    sedes_pesquisadas =  list(interc_s) + search_sedes_nome
+    sedes_pesquisadas = list(interc_s) + search_sedes_nome
 
     #resultados de novidades pesquisadas
     search_novidades_titulo = Novidade.query.filter(Novidade.title.contains(str(pesquisa).strip().title())).all() 
@@ -75,8 +83,12 @@ def pesquisa():
     autor_n = set(search_novidades_autor)
     texto_n = set(search_novidades_texto)
 
-    interc_n = (texto_n - autor_n) - titulo_n
-    novidades_pesquisadas =  list(interc_n) + search_novidades_titulo
+    interc_n1 = texto_n - titulo_n
+    title_no_repeat = list(interc_n1) + search_novidades_titulo
+    titulo_e_texto_n = set(title_no_repeat)
+
+    interc_n2 = titulo_e_texto_n - autor_n
+    novidades_pesquisadas = list(interc_n2) + search_novidades_autor
 
     # ÚLTIMA SEDE PARA RETIRAR A LINHA
     if sedes_pesquisadas:
